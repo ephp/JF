b26e7548bc0a8b1544e7c04544b9db6f27feb6fb
Ephraim <ephraim.pepe@gmail.com>
2013-10-06 15:47:45 +0200

    ad

--- web/app_dev.php
+++ web/app_dev.php
@@ -3,14 +3,10 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
 
-if($_REQUEST['ephraim'] == 'livorno') {
-    $_SESSION['ephraim'] = true;
-}
-
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#configuration-and-setup for more information
 //umask(0000);
-
+/*
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
 if (!isset($_SESSION['ephraim'])
@@ -21,7 +17,7 @@ if (!isset($_SESSION['ephraim'])
     header('HTTP/1.0 403 Forbidden');
     exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
-
+*/
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 Debug::enable();
 

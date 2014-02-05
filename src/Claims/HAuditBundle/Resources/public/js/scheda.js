ae9284ced1a913c97e4bf51f7c11ba90923db4bc
Ephraim <staf@jf-system.com>
2014-02-05 11:13:59 +0100

    fix currecy

--- src/Claims/HAuditBundle/Resources/public/js/scheda.js
+++ src/Claims/HAuditBundle/Resources/public/js/scheda.js
@@ -30,9 +30,9 @@ function _autoupdate($this) {
     val = $this.val();
     pratica = $this.attr('pratica');
     field = $this.attr('name');
-        $.post(Routing.generate('claims-h-audit-autoupdate', {'slug': pratica}), {'pratica': {'field': field, 'value': val}}, function(out) {
-            console.log(out);
-        });
+    $.post(Routing.generate('claims-h-audit-autoupdate', {'slug': pratica}), {'pratica': {'field': field, 'value': val}}, function(out) {
+        console.log(out);
+    });
 }
 
 function autoupdate() {
@@ -40,10 +40,10 @@ function autoupdate() {
 //        _autoupdate($(this));
 //    });
     var old = null;
-    $('.autoupdate').focus(function(){
+    $('.autoupdate').focus(function() {
         old = $(this).val();
     }).blur(function() {
-        if($(this).val() !== old) {
+        if ($(this).val() !== old) {
             _autoupdate($(this));
         }
         old = null;
@@ -55,7 +55,7 @@ function sanitizePartialDate(fields) {
     fields.forEach(function(field) {
         field.change(function() {
             value = $(this).val().toLowerCase();
-            if(value === '') {
+            if (value === '') {
                 return false;
             }
             d = new Date();
@@ -77,36 +77,36 @@ function sanitizePartialDate(fields) {
                         d = calcolaData(d, nd);
                         break;
                 }
-                g = d.getUTCDate() < 10 ? '0'+d.getUTCDate() : d.getUTCDate();
-                m = d.getUTCMonth() < 9 ? '0'+(d.getUTCMonth() + 1) : (d.getUTCMonth() + 1);
+                g = d.getUTCDate() < 10 ? '0' + d.getUTCDate() : d.getUTCDate();
+                m = d.getUTCMonth() < 9 ? '0' + (d.getUTCMonth() + 1) : (d.getUTCMonth() + 1);
                 a = d.getUTCFullYear();
-                $(this).val(g+'/'+m+'/'+a);
+                $(this).val(g + '/' + m + '/' + a);
             } else {
                 numeri = $(this).val().replace(/\-/g, "/").replace(/\./g, "/").replace(/\//g, " ").words();
-                if(numeri.length === 3) {
+                if (numeri.length === 3) {
                     d.setUTCDate(parseInt(numeri[0], 10));
                     d.setUTCMonth(parseInt(numeri[1], 10) - 1);
                     d.setUTCFullYear(parseInt(numeri[2], 10) < 100 ? 2000 + parseInt(numeri[2], 10) : parseInt(numeri[2], 10));
-                } 
-                if(numeri.length === 2) {
+                }
+                if (numeri.length === 2) {
                     d.setUTCDate(1);
                     d.setUTCMonth(parseInt(numeri[0], 10) - 1);
                     d.setUTCFullYear(parseInt(numeri[1], 10) < 100 ? 2000 + parseInt(numeri[1], 10) : parseInt(numeri[1], 10));
-                } 
-                if(numeri.length === 1) {
+                }
+                if (numeri.length === 1) {
                     d.setUTCDate(1);
                     d.setUTCMonth(0);
                     d.setUTCFullYear(parseInt(numeri[0], 10) < 100 ? 2000 + parseInt(numeri[0], 10) : parseInt(numeri[0], 10));
-                } 
-                g = parseInt(d.getUTCDate()) < 10 ? '0'+d.getUTCDate() : d.getUTCDate();
-                m = parseInt(d.getUTCMonth()) + 1 < 10 ? '0'+(parseInt(d.getUTCMonth()) + 1) : (parseInt(d.getUTCMonth()) + 1);
+                }
+                g = parseInt(d.getUTCDate()) < 10 ? '0' + d.getUTCDate() : d.getUTCDate();
+                m = parseInt(d.getUTCMonth()) + 1 < 10 ? '0' + (parseInt(d.getUTCMonth()) + 1) : (parseInt(d.getUTCMonth()) + 1);
                 a = d.getUTCFullYear();
-                if(numeri.length === 1) {
+                if (numeri.length === 1) {
                     $(this).val(a);
-                } else if(numeri.length === 2) {
-                    $(this).val(m+'/'+a);
+                } else if (numeri.length === 2) {
+                    $(this).val(m + '/' + a);
                 } else {
-                    $(this).val(g+'/'+m+'/'+a);
+                    $(this).val(g + '/' + m + '/' + a);
                 }
             }
         });
@@ -115,30 +115,40 @@ function sanitizePartialDate(fields) {
 
 function sanitizeCurrencyFormat(fields) {
     fields.forEach(function(field) {
+        field.each(function() {
+            _sanitizeCurrencyFormat($(this));
+        });
         field.change(function() {
-            value = $(this).val().replace(",", ".").remove(/[^0-9\.]/g);
-            n = 0;
-            i = 0;
-            nc = 0;
-            value.chars(function(c) {
-                if (c === '.') {
-                    n++;
-                }
-                if (n === 2) {
-                    i = nc;
-                    n++;
-                }
-                nc++;
-            });
-            if (n > 1) {
-                value = value.substring(0, i);
-                value = Math.abs(parseFloat(value === '' || value === '.' ? 0 : value));
-                $(this).val(value.toFixed(2));
-            } else {
-                value = Math.abs(parseFloat(value === '' || value === '.' ? 0 : value));
-                $(this).val(value.toFixed(2));
-            }
-            $(this).val(parseFloat($(this).val()).format(2, '.', ','));
+            _sanitizeCurrencyFormat($(this));
         });
     });
+}
+
+function _sanitizeCurrencyFormat(field) {
+    if(!field.val()) {
+        return;
+    }
+    value = field.val().replace(",", ".").remove(/[^0-9\.]/g);
+    n = 0;
+    i = 0;
+    nc = 0;
+    value.chars(function(c) {
+        if (c === '.') {
+            n++;
+        }
+        if (n === 2) {
+            i = nc;
+            n++;
+        }
+        nc++;
+    });
+    if (n > 1) {
+        value = value.substring(0, i);
+        value = Math.abs(parseFloat(value === '' || value === '.' ? 0 : value));
+        field.val(value.toFixed(2));
+    } else {
+        value = Math.abs(parseFloat(value === '' || value === '.' ? 0 : value));
+        field.val(value.toFixed(2));
+    }
+    field.val(parseFloat(field.val()).format(2, '.', ','));
 }
